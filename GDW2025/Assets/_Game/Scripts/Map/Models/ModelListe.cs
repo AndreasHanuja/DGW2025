436258b9f2052ea821cfc0f3cbf30cbb5e16a933
Andreas Hanuja <hanuja2@gmail.com>
2025-03-12 15:50:10 +0100

    load model in start

--- GDW2025/Assets/_Game/Scripts/Map/Models/ModelListe.cs
+++ GDW2025/Assets/_Game/Scripts/Map/Models/ModelListe.cs
@@ -20,6 +20,7 @@ public class ModelListe : MonoBehaviour
 
         foreach (PlyModelSetup setup in models)
         {
+            setup.LoadModel();
             for (int i = 0; i < 8; i++)
             {
                 prefabs.Add(setup.createdPrefabs[i]);
